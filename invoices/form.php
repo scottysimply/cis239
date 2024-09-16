ab8d145c379db9fb56294d96f2bf6cadcd611dc0
scottsimply <scottistriggered@gmail.com>
2024-09-16 08:17:47 -0500

    Initial commit

---
+++ invoices/form.php
@@ -0,0 +1,5 @@
+<form>
+    <input type="date" id="invoice-date" name="invoiceDate"></input>
+    <input type="date" id="expiry-date" name="expiryDate"></input>
+    <input type="submit">Submit</input>
+</form>
