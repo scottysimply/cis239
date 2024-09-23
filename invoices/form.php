a3c594dfd9d0a445e72d417178654a0b89a16c5d
scottsimply <scottistriggered@gmail.com>
2024-09-23 08:15:57 -0500

    Added labels to the form

--- invoices/form.php
+++ invoices/form.php
@@ -1,5 +1,7 @@
 <form>
+    <label for="invoice-date">Date of Invoice: </label>
     <input type="date" id="invoice-date" name="invoiceDate"></input>
+    <label for="expiry-date">Expiration Date: </label>
     <input type="date" id="expiry-date" name="expiryDate"></input>
-    <input type="submit">Submit</input>
+    <input type="submit"></input>
 </form>
