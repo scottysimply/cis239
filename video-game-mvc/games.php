c5584b6a2a96dfe1b46a557baef6a2ced733c7d3
scottsimply <scottistriggered@gmail.com>
2024-10-21 10:08:00 -0500

    Working on retrieving data from a database instead of csv

--- video-game-mvc/games.php
+++ video-game-mvc/games.php
@@ -1,13 +1,83 @@
 <?php
-        // Obtain games from csv
-        $games = [];
-        $file = fopen($_SERVER['DOCUMENT_ROOT'] . "/cis239/video-game/data/games.csv", "rb");
-        while (!feof($file)) {
-            // Add to data
-            array_push($games, fgetcsv($file));
+        class Game {
+            public int $id;
+            public string $name;
+            public array $platforms;
+            public array $genres;
+            public string $image_path;
+            public function __construct() {
+
+            }
+            public static function newGame(int $id, string $name, array $platform, array $genre, string $image): Game {
+                $game = new Game();
+                $game->id = $id;
+                $game->name = $name;
+                $game->platforms = $platform;
+                $game->genres = $genre;
+                $game->image_path = $image;
+                return $game;
+            }
         }
-        // close stream
-        fclose($file);
+        // Obtain games from database
+        $games = [];
+        // Query database for games
+        try {
+            $db = new PDO("mysql:host=localhost;dbname=sp0989061", "root", "");
+        
+            /*
+             * ORDER OF QUERIES
+             * get games
+             * get relations between game id and genre id
+             * get relations between game id and platform id
+             * get relations between game id and image id
+             * at the very end, we can map IDs to the actual data
+             */
+            $game_query = 
+           "SELECT game.game_id, game.game_title
+            FROM vg_games AS game
+            ";
+
+            $genre_query =
+           "SELECT game.game_id, genre.genre_name
+            FROM vg_games AS game
+            JOIN vg_game_genres AS genre ON game.game_id = genre.game_id
+            JOIN vg_genre AS g ON genre.genre_id = g.genre_id;
+            ";
+
+            $platform_query = 
+           "SELECT game.game_id, platform.platform_name
+            FROM vg_games AS game
+            JOIN vg_game_platforms AS platform ON game.game_id = platform.game_id
+            JOIN vg_platform AS p ON platform.platform_id = p.platform_id;
+            ";
+
+            $image_query = 
+           "SELECT game.game_id, image.image_path
+            FROM vg_games AS game
+            JOIN vg_game_images AS img ON game.game_id = img.game_id
+            JOIN vg_images AS i ON img.image_id = i.image_id;
+            ";
+
+            // Do the queries
+            $game_titles = $db->query($game_query)->fetchAll();
+            $game_genres = $db->query($genre_query)->fetchAll();
+            $game_platforms = $db->query($platform_query)->fetchAll();
+            $game_images = $db->query($image_query)->fetchAll();
+
+            // Map results to our object
+            $addingData = true;
+            while ($addingData) {
+                $game = new Game();
+                for
+            }
+        
+        }
+        catch (PDOException $e) {
+        }
+    
+    
+    
+    
     ?>
 <!--Card container-->
 <div class="container">
