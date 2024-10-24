fce064cc0e7bf7adaa39888c3180e296ae3f6445
scottsimply <scottistriggered@gmail.com>
2024-10-24 09:20:50 -0500

    Updated style for addgame.php

--- video-game-mvc/addgame.php
+++ video-game-mvc/addgame.php
@@ -1,13 +1,13 @@
 <?php
     include_once 'controller.php';
 ?>
-<div class="container mt-5 col-5 col-3-md">
+<div class="container mt-5 col-5 col-3-md mb-5">
     <h3>Add Game</h3>
     <form method="post" class="form border p-2 border-primary-subtle rounded" enctype="multipart/form-data">
         <!--This input is what i use to know if the form was submitted. This is something i picked up from asp.net-->
         <input value="true" name="submitted" hidden/>
         <label for="game-title">Title: </label>
-        <input type="text" name="gameTitle" id="game-title" class="mb-2 w-100"><br/>
+        <input type="text" name="gameTitle" id="game-title" class="w-100 form-control"><br/>
         <?php
             if (isset($_POST['submitted']) && !$gameData->titleValidated) {
                 $error_msg = "";
@@ -21,7 +21,7 @@
             }
         ?>
         <label for="game-genre">Genre: </label>
-        <input type="text" name="gameGenre" id="game-genre" class="mb-2 w-100"><br/>
+        <input type="text" name="gameGenre" id="game-genre" class="w-100 form-control"><br/>
         <?php
             if (isset($_POST['submitted']) && !$gameData->genreValidated) {
                 $error_msg = "";
@@ -35,7 +35,7 @@
             }
         ?>
         <label for="game-platform">Platform: </label>
-        <input type="text" name="gamePlatform" id="game-platform" class="mb-2 w-100"><br/>
+        <input type="text" name="gamePlatform" id="game-platform" class="w-100 form-control"><br/>
         <?php
             if (isset($_POST['submitted']) && !$gameData->platformValidated) {
                 $error_msg = "";
@@ -49,7 +49,7 @@
             }
         ?>
         <label for="game-image">Image: </label>
-        <input type="file" name="gameImage" id="game-image" class="mb-2"><br/>
+        <input type="file" name="gameImage" id="game-image" class="form-control"><br/>
         <?php
             if (isset($_POST['submitted']) && !$gameData->imagePathValidated) {
                 $error_msg = "The image could not be added.";
